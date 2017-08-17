07ecf7ea96603adfca4baa6e44028af9fc6d68af
Kyle Erwin <kyle.erwin24@gmail.com>
2017-08-17 18:26:17 +0200

    Updates

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './register.component.html'
+})
+export class RegisterComponent {
+  title = 'Register';
+}
+
---
+++ src/app/register/register.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RegisterComponent } from './register.component';
+
+@NgModule({
+  declarations: [
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [RegisterComponent]
+})
+export class RegisterModule { }
