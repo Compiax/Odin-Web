b938b780c5c40509276124d52252d78be1b694bb
Kyle Erwin <kyle.erwin24@gmail.com>
2017-08-18 12:53:36 +0200

    Removed unessisary files

--- src/app/home/documents-page/document-display-box/document-display-box.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { TabsBarComponent } from './../../tabs-bar/tabs-bar.component'
-
-@Component({
-  selector: 'app-document-display-box',
-  templateUrl: './document-display-box.component.html',
-  styleUrls: ['./document-display-box.component.scss']
-})
-export class DocumentDisplayBoxComponent {}
