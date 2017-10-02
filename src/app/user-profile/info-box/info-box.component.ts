001b66c44275ec9e17cd48df1613424a61e63c10
Kyle Erwin <kyle.erwin24@gmail.com>
2017-10-02 18:16:07 +0200

    info box added

---
+++ src/app/user-profile/info-box/info-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-info-box',
+  templateUrl: './info-box.component.html',
+  styleUrls: ['./info-box.component.scss'],
+})
+
+export class InfoBoxComponent implements OnInit {
+  ngOnInit() {
+  }
+}
