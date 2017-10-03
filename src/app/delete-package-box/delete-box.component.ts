3570d6f78ae32551210b846e5c31378543c90642
Kyle Erwin <kyle.erwin24@gmail.com>
2017-10-03 12:49:42 +0200

    Delete box ts file updated with getting package

--- src/app/delete-package-box/delete-box.component.ts
+++ src/app/delete-package-box/delete-box.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute } from '@angular/router';
+import { ComponentsService } from '../_services/components.service';
 import { Component, OnInit} from '@angular/core';
 
 @Component({
@@ -7,6 +9,40 @@ import { Component, OnInit} from '@angular/core';
 })
 
 export class DeletePackageBoxComponent implements OnInit {
-  ngOnInit() {
+  public package: any;
+  private id: String;
+  private sub: any;
+
+  constructor(private componentsService: ComponentsService, private route: ActivatedRoute) { 
+    this.package = {};
   }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+		console.log('Fetching package with id ' + params['id']);
+		this.loadComponents(params['id']);
+   });
+  }
+
+  delete() {
+	  // to do 
+  }
+
+  loadComponents(id: String) {
+    let self = this;
+    this.componentsService.getComponent(id)
+    .then((res: any) => {
+      let data = JSON.parse(res._body).data[0];
+      console.log(data);
+      console.log(self.package);
+      self.package.name = data.attributes.name;
+      self.package.author = data.attributes.author.username;
+      self.package.usage = data.attributes.usage;
+      self.package.stats = data.attributes.stats;
+      self.package.created = data.attributes.created;
+      self.package.description = data.attributes.description;
+      console.log(self.package);
+    });
+  };
+
 }
