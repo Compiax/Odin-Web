0408a88b6993cf57fafd430a5b9fea6d763cb95a
Kyle Erwin <kyle.erwin24@gmail.com>
2017-10-03 10:57:37 +0200

    Search box and user profile changes

---
+++ src/app/user-profile/projects-box/projects-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-project-box',
+  templateUrl: './projects-box.component.html',
+  styleUrls: ['./projects-box.component.scss'],
+})
+
+export class ProjectBoxComponent implements OnInit {
+  ngOnInit() {
+  }
+}
--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -6,12 +6,16 @@ import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileComponent } from './user-profile.component';
 import { ProfileBoxComponent } from './profile-box/profile-box.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
+import { StatsBoxComponent } from './stats-box/stats-box.component';
+import { ProjectBoxComponent } from './projects-box/projects-box.component';
 
 @NgModule({
   declarations: [
       UserProfileComponent,
       ProfileBoxComponent,
-      InfoBoxComponent
+      InfoBoxComponent,
+      StatsBoxComponent,
+      ProjectBoxComponent
   ],
   imports: [
     CommonModule,
