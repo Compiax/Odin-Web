c9e5fc1e57927287035d4b6d52864384f3444b89
Kyle Erwin <kyle.erwin24@gmail.com>
2017-10-03 12:39:47 +0200

    Delete box added

---
+++ src/app/delete-package-box/delete-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-delete-package-box',
+  templateUrl: './delete-package-box.component.html',
+  styleUrls: ['./delete-package-box.component.scss'],
+})
+
+export class DeletePackageBoxComponent implements OnInit {
+  ngOnInit() {
+  }
+}
