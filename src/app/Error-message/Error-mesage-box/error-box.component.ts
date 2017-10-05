9cf8d574b0cede8db3a6d9db91e8fbeaca2ddf35
Kyle Erwin <kyle.erwin24@gmail.com>
2017-10-05 16:59:20 +0200

    error box init]

---
+++ src/app/Error-message/Error-mesage-box/error-box.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+
+@Component({
+  selector: 'app-error-box',
+  templateUrl: './error-box.component.html',
+  styleUrls: ['./error-box.component.scss'],
+  providers: []
+})
+
+export class ErrorBoxComponent implements OnInit {
+
+  constructor(public errorNumber: number, public message: string) {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
